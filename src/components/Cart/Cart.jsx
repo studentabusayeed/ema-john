c471360769d2af95c738d3dc9a4738422d71c4bd
studentAbuSayeed <studentabusayeed2@gmail.com>
2023-03-28 19:53:54 +0600

    save cart to local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
+    // const cart = props.cart; // option-1
+    // const {cart} = props; // option-2
+
+    console.log(cart);
+
     let totalPrice = 0;
     let totalShipping = 0;
-    for(const product of cart){
+    for (const product of cart) {
         totalPrice = totalPrice + product.price;
         totalShipping = totalShipping + product.shipping;
     }
 
-    const tax = totalPrice*7/100;
+    const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + totalShipping + tax;
-    // const cart = props.cart; // option-1
-    // const {cart} = props; // option-2
+
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
